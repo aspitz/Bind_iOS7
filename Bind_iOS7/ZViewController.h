4063b17554af17687b8a6233f0441cd42e2b20be
Ayal Spitz <aspitz@patientkeeper.com>
2013-08-26 11:55:32 -0400

    Initial Commit

---
+++ Bind_iOS7/ZViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZViewController.h
+//  Bind_iOS7
+//
+//  Created by Ayal Spitz on 8/26/13.
+//  Copyright (c) 2013 spitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZViewController : UIViewController
+
+@end
---
+++ Bind_iOS7Tests/Bind_iOS7Tests.m
@@ -0,0 +1,34 @@
+//
+//  Bind_iOS7Tests.m
+//  Bind_iOS7Tests
+//
+//  Created by Ayal Spitz on 8/26/13.
+//  Copyright (c) 2013 spitz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bind_iOS7Tests : XCTestCase
+
+@end
+
+@implementation Bind_iOS7Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
